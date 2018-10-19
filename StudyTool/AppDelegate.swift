adc6d5085b54ec933024fd4d5870f492cfe02376
Aaron Walker <aaron45587@gmail.com>
2018-10-19 18:36:59 +0100

    Initial Commit

---
+++ StudyTool/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  StudyTool
+//
+//  Created by Aaron Walker on 19/10/2018.
+//  Copyright © 2018 Aaron Walker. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ StudyTool/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  StudyTool
+//
+//  Created by Aaron Walker on 19/10/2018.
+//  Copyright © 2018 Aaron Walker. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
