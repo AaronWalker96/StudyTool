777d8d2b1a1907a3225b3ca3618dc71d05816d75
Aaron Walker <aaron45587@gmail.com>
2018-10-20 23:54:46 +0100

    Added preferences window

---
+++ StudyTool/PrefsViewController.swift
@@ -0,0 +1,33 @@
+//
+//  PrefsViewController.swift
+//  StudyTool
+//
+//  Created by Aaron Walker on 20/10/2018.
+//  Copyright © 2018 Aaron Walker. All rights reserved.
+//
+
+import Cocoa
+
+class PrefsViewController: NSViewController {
+
+    @IBOutlet weak var presetsPopup: NSPopUpButton!
+    @IBOutlet weak var customSlider: NSSlider!
+    @IBOutlet weak var customTextField: NSTextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+    @IBAction func popupValueChanged(_ sender: Any) {
+    }
+    
+    @IBAction func sliderValueChanged(_ sender: Any) {
+    }
+    
+    @IBAction func cancelButtonClicked(_ sender: Any) {
+    }
+    
+    @IBAction func okButtonClicked(_ sender: Any) {
+    }
+}
